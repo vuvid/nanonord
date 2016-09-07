7045370b4a0758e0cb1a3b20bebf930fb28e8fc3
Vujadin Vidovic <vv@clienti.dk>
2016-09-07 12:23:55 +0200

    Add project files.

---
+++ nanonord/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nanonord.Startup))]
+namespace nanonord
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
